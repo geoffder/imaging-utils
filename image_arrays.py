d7b502187ef3b10053f0cab0013710443f7afb94
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-02-16 17:26:39 -0800

    Move torch dependent function out of image_arrays

--- image_arrays.py
+++ image_arrays.py
@@ -8,8 +8,6 @@ from skimage import io
 from scipy import signal
 from PIL import Image
 from tifffile import imsave
-import torch
-import torch.nn.functional as F
 """
 NOTE: Discoveries in working with suite2p again.
 - Can provide data as h5 archives with a stack stored under the key "data"
@@ -353,15 +351,6 @@ def re_export_folder(pth):
         if not (fname.endswith(".tiff") or fname.endswith(".tif")):
             continue
         imsave(os.path.join(out_pth, fname), io.imread(os.path.join(pth, fname)))
-
-
-def pool_tiff(pth, fname, kernel_size=2, stride=None, padding=0):
-    arr = io.imread(os.path.join(pth, fname))
-    t = torch.from_numpy(arr.astype(float))
-    pooled = F.avg_pool2d(t, kernel_size, stride, padding).numpy()
-    u16 = normalize_uint16(remove_offset(pooled))
-    label = "pooled_%i" % kernel_size
-    array_to_tiff(os.path.join(pth, label), label, u16)
 
 
 def simple_upsample_2D(arr, y=1, x=1):
