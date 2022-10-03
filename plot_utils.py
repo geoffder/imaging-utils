f6f09cbf271a257ffaafd1a76ef6e6961f55e691
Geoff deRosenroll <geoffderosenroll@gmail.com>
2022-10-03 15:44:43 -0700

    Grid ROI biphasic metrics

--- plot_utils.py
+++ plot_utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def clean_axes(axes, remove_spines=["right", "top"], ticksize=11):
     """A couple basic changes I often make to pyplot axes. If input is an
     iterable of axes (e.g. from plt.subplots()), apply recursively."""
@@ -17,3 +20,48 @@ def clean_axes(axes, remove_spines=["right", "top"], ticksize=11):
             ticks.set_fontsize(ticksize)
         for ticks in axes.get_xticklabels():
             ticks.set_fontsize(ticksize)
+
+
+def simple_beeswarm(y, nbins=None):
+    """
+    Returns x coordinates for the points in ``y``, so that plotting ``x`` and
+    ``y`` results in a bee swarm plot.
+    Copied from https://stackoverflow.com/a/71498646
+    """
+    y = np.asarray(y)
+    if nbins is None:
+        nbins = len(y) // 6
+
+    # Get upper bounds of bins
+    x = np.zeros(len(y))
+    ylo = np.min(y)
+    yhi = np.max(y)
+    dy = (yhi - ylo) / nbins
+    ybins = np.linspace(ylo + dy, yhi - dy, nbins - 1)
+
+    # Divide indices into bins
+    i = np.arange(len(y))
+    ibs = [0] * nbins
+    ybs = [0] * nbins
+    nmax = 0
+    for j, ybin in enumerate(ybins):
+        f = y <= ybin
+        ibs[j], ybs[j] = i[f], y[f]
+        nmax = max(nmax, len(ibs[j]))
+        f = ~f
+        i, y = i[f], y[f]
+    ibs[-1], ybs[-1] = i, y
+    nmax = max(nmax, len(ibs[-1]))
+
+    # Assign x indices
+    dx = 1 / (nmax // 2)
+    for i, y in zip(ibs, ybs):
+        if len(i) > 1:
+            j = len(i) % 2
+            i = i[np.argsort(y)]
+            a = i[j::2]
+            b = i[j + 1 :: 2]
+            x[a] = (0.5 + j / 3 + np.arange(len(b))) * dx
+            x[b] = (0.5 + j / 3 + np.arange(len(b))) * -dx
+
+    return x
